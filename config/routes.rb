9ee932eb4cfe28d9357d94fa26cd22bbaade4a58
gwho <ahnbizcad@gmail.com>
2014-01-26 23:35:06 -0800

    3.5 advanced setup and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Mostlystatic::Application.routes.draw do
   get 'home', to: 'static_pages#home', as: :home
   get 'help', to: 'static_pages#help', as: :help
   get 'about', to: 'static_pages#about', as: :about
-
+  get 'contact', to: 'static_pages#contact', as: :contact
+  
   #match 'about' to: '/static_pages/about', as: :about, via: :all
 
   # The priority is based upon order of creation: first created -> highest priority.
